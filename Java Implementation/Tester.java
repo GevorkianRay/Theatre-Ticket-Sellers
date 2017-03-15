8001642fc821b0bd8aa45b4dbd86a2a9a5db88c4
nglawrence415 <ng.lawrence415@gmail.com>
2017-03-15 15:36:50 -0700

    Update Tester.java

--- Java Implementation/Tester.java
+++ Java Implementation/Tester.java
@@ -1,1 +1,25 @@
 
+import java.util.Scanner;
+
+public class Tester {
+	
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Enter # of customers:");// gets the number of customers
+		Scanner scan=new Scanner(System.in);		//for each seller
+		int numCustomers=scan.nextInt();
+		scan.close();
+		
+		seats seat=new seats();
+		
+		
+		double startTime=System.currentTimeMillis();
+		Thread.sleep(5000);
+		double elapsed = System.currentTimeMillis();
+		float seconds = (float) ((elapsed - startTime) / 1000F);
+		System.out.println(Float.toString(seconds) + " seconds.");
+	
+	}
+	
+}
