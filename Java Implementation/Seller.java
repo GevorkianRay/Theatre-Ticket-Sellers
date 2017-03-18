7a0976da6522943443471505a005169d0640161b
nglawrence415 <ng.lawrence415@gmail.com>
2017-03-18 00:13:59 -0700

    Update Seller.java

--- Java Implementation/Seller.java
+++ Java Implementation/Seller.java
@@ -1,33 +1,88 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
-public class Seller{
-	private String ticketPrice; //H,M,L sellers
-	private int id; //seller id
-	private int n; //input 
-	Queue<Customer> line = new LinkedList<Customer>();
+import java.util.*;
+public class Seller implements Runnable {
+	 ArrayList<Customer> listOfCust ;
+	 Timer t;
+	 int numTicketsSold;
+	 int totalCustomers;
+	 int sellingTime;
+	 int max;
+	 int min;
+	 seats map;
+	 
+	 
+	 
+	 public Seller(int numCustomers, seats mapOfSeats) {
+		 map=mapOfSeats;
+		 totalCustomers=numCustomers;
+		 t=new Timer();
+		 numTicketsSold=0;
+		 listOfCust= new ArrayList<Customer>();
+		 max=0;
+		 min =0 ;
+		 for(int i = 0 ; i < numCustomers;i++) {
+			 
+			 listOfCust.add(new Customer((int)(Math.random()*59)));
+		 }
+		 
+		 Collections.sort(listOfCust, new customerArrivalCompare());
+		 
+		 
+	 }
+	 /*
+	  * makes list of customers in sorted order
+	  * 
+	  */
+	 static class customerArrivalCompare implements Comparator<Customer> {
+			public int compare(Customer arg0, Customer arg1) {
+				// TODO Auto-generated method stub
+				return arg0.compareTo(arg1);
+			}
+		}
+	 
+	public void run() {
+		
+		while(t.minutesElapsed<60 && numTicketsSold<totalCustomers) {
+			t.run();
+			Customer current=listOfCust.get(0);
+			if(shouldSell(t.minutesElapsed,current)) {
+				sellTicket(max, min);
+			}
+			
+		}
+		
+	}
 	
-	public Seller(String ticketPrice, int id, int n){
-		this.ticketPrice = ticketPrice;
-		this.id = id;
-		this.n = n;
+	/*
+	 * checks if the customer is here at the right time
+	 */
+	public boolean shouldSell(int currentMinute, Customer currentCustomer) {
+		if(currentMinute>=currentCustomer.arrivalT) {
+			
+			return true;
+		}
+		return false;
 	}
-	public void setPrice(String p){
-		ticketPrice = p;
+	/*
+	 * sells ticket to customers
+	 */
+	public void sellTicket(int min1, int max1) {
+		int range = max1-min1+1;
+		sellingTime=min1 +(int)((Math.random()*range));
+		
+		
+		
 	}
-	public String getPrice(){
-		return ticketPrice;
-	}
-	public void setID(int id){
-		this.id = id;
-	}
-	public int getID(){
-		return id;
-	}
-	public void addCustomer(Customer c){
-		if(line.size() < n){
-			line.add(c);
+	
+	
+	/*
+	 * prints customers in list
+	 * 
+	 */
+	public void printMyCustomers() {
+		Iterator i = listOfCust.iterator();
+		while(i.hasNext()) {
+			Customer current=(Customer) i.next();
+			System.out.println(current.arrivalT);
 		}
 	}
-	
 }
