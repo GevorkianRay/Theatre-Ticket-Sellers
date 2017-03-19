694cf0ad347badafc246f34cb078da44b61d19b8
nglawrence415 <ng.lawrence415@gmail.com>
2017-03-19 12:19:05 -0700

    Add files via upload

---
+++ Java Implementation/LSeller.java
@@ -0,0 +1,50 @@
+public class LSeller extends Seller {
+	
+	public LSeller(int numCust,seats a, String sellerName,Timer timer) {
+		super(numCust,a,sellerName,timer);
+		max=7;
+		min=4;
+		row = 9;
+		
+	}
+	public void sellTicket(int min1, int max1,Customer c) {
+		Timer a = new Timer();
+		int currentTime=a.getTime();
+		sellingTime=(int)(Math.random()*4 + 4);
+		System.out.println(name + " takes " + sellingTime + " mins");
+		while(a.getTime()<sellingTime) {
+			//System.out.println("i am selling" + a.getTime()  + "\t " + sellingTime);//do nothing until it is time to sell
+		}
+		if(!listOfCust.isEmpty()) {
+		checkSeatMap(c);
+		}
+		
+		
+	}
+	
+	public synchronized void checkSeatMap(Customer cust) {
+		Customer c=listOfCust.remove(0); //remove first customer on list
+		boolean x = true;
+		for(int ro = 9 ;ro > 0 && x; ro--) {
+			for(int co=0; co < 10 && x; co++ ) {
+				if(!map.isSold(ro, co)) {
+					x=false;
+					numTicketsSold++;
+					map.seat[ro][co]=c.name;
+					try {
+		                Thread.sleep(10);
+		            } catch (InterruptedException e) {
+		                Thread.currentThread().interrupt();
+		            }
+					System.out.println();
+					System.out.println();
+					System.out.println();
+					map.toString();
+				}
+			}
+			
+		
+		}
+	}
+
+}
