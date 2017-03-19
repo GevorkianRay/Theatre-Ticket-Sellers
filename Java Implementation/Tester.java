3e6c6a5ae5e82ff6333cc299bdb8dc06a4322fb9
nglawrence415 <ng.lawrence415@gmail.com>
2017-03-18 18:16:19 -0700

    Update Tester.java

--- Java Implementation/Tester.java
+++ Java Implementation/Tester.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Tester {
@@ -5,23 +6,31 @@ public class Tester {
 	
 	
 	public static void main(String[] args) throws InterruptedException {
-		
-		int ticketsSold=0;
-		
-		System.out.println("Enter # of customers:");// gets the number of customers
 
+		System.out.println("Enter # of customers:");// gets the number of customers
 		Scanner scan=new Scanner(System.in);		//for each seller
 		int numCustomers=scan.nextInt();
 		scan.close();
-		
 		seats seat=new seats();
-		
 		Timer t = new Timer();
 		
-		while(t.minutesElapsed<60 && ticketsSold<100 && ticketsSold < 10*numCustomers) {
-			
-			
-		}
+		HSeller H1 = new HSeller(numCustomers,seat,"H1");
+		LSeller L2 = new LSeller(numCustomers,seat,"L2");
+		LSeller L3 = new LSeller(numCustomers,seat,"L3");
+		LSeller L4 = new LSeller(numCustomers,seat,"L4");
+		LSeller L5 = new LSeller(numCustomers,seat,"L5");
+		LSeller L6 = new LSeller(numCustomers,seat,"L6");
+ArrayList<Customer> Test= new ArrayList<Customer>();
+Test.add(new Customer(0,"H11"));
+
+H1.listOfCust=Test;
+		
+H1.printMyCustomers();
+	H1.run();
+
+		
+		
+	
 	
 	}
 	
