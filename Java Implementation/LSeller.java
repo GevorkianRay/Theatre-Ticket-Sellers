edd694cf4a3096c8fedf1aaf005a429b058d0e5a
Michael-Huang-9001 <Michael.Huang.9001@gmail.com>
2017-03-21 16:39:02 -0700

    Add files via upload

--- Java Implementation/LSeller.java
+++ Java Implementation/LSeller.java
@@ -1,18 +1,23 @@
 /**
- * L Seller is a type of Seller.
- * It sells within a specific row pattern and takes the longest time to serve.
+ * L Seller is a type of Seller. It sells within a specific row pattern and
+ * takes the longest time to serve.
  */
 public class LSeller extends Seller {
 
 	/**
-
+	 * 
 	 * Constructor for the M Seller.
-	 * @param numberOfCustomers - the maximum number of customers to serve.
-	 * @param seats             - the seats that the L seller can serve.
-	 * @param sellerName        - the name of the current L Seller.
-	 * @param timer             - the timer to track the L seller.
+	 * 
+	 * @param numberOfCustomers
+	 *            - the maximum number of customers to serve.
+	 * @param seats
+	 *            - the seats that the L seller can serve.
+	 * @param sellerName
+	 *            - the name of the current L Seller.
+	 * @param timer
+	 *            - the timer to track the L seller.
 	 */
-	public LSeller(int numberOfCustomers,Seats seats, String sellerName, Timer timer) {
+	public LSeller(int numberOfCustomers, Seats seats, String sellerName, Timer timer) {
 		super(numberOfCustomers, seats, sellerName, timer);
 		maxTimeToServe = 7;
 		minTimeToServe = 4;
@@ -21,42 +26,50 @@ public class LSeller extends Seller {
 
 	/**
 	 * Method to sell a ticket to the given customer.
-	 * @param minServeTime - the minimum time to serve a customer.
-	 * @param maxServeTime - the maximum time to serve a customer.
-	 * @param customer     - the customer to sell a ticket to.
+	 * 
+	 * @param minServeTime
+	 *            - the minimum time to serve a customer.
+	 * @param maxServeTime
+	 *            - the maximum time to serve a customer.
+	 * @param customer
+	 *            - the customer to sell a ticket to.
 	 */
 	public void sellTicket(int minServeTime, int maxServeTime, Customer customer) {
 		Timer timer = new Timer();
-		sellingTime =(int)(Math.random()* 4 + 4);
+		sellingTime = (int) (Math.random() * 4 + 4);
 		System.out.println(name + " takes " + sellingTime + " mins");
-		while(timer.getElapsedTime() < sellingTime) {} // Do nothing until it is time to sell.
-		if(!listOfCustomers.isEmpty()) {
-		checkSeatMap(customer);
+		while (timer.getElapsedTime() < sellingTime) {
+		} // Do nothing until it is time to sell.
+		if (!listOfCustomers.isEmpty()) {
+			checkSeatMap(customer);
 		}
 	}
 
 	/**
 	 * Checks the seating map with synchronization between sellers.
-	 * @param customer - the customer to place into the seating map.
+	 * 
+	 * @param customer
+	 *            - the customer to place into the seating map.
 	 */
 	public synchronized void checkSeatMap(Customer customer) {
 		boolean x = true;
-		for(int ro = 9; ro > 0 && x; ro--) {
-			for(int co = 0; co < 10 && x; co++ ) {
-				if(!map.isSold(ro, co)) {
+		for (int row = 9; row > 0 && x; row--) {
+			for (int col = 0; col < 10 && x; col++) {
+				if (!map.isSold(row, col)) {
 					x = false;
-					listOfCustomers.remove(0);
+					Customer c = listOfCustomers.remove(0);
+					// listOfCustomers.remove(0);
 					numTicketsSold++;
-					map.seats[ro][co] = customer.customerName;
+					map.seats[row][col] = customer.customerName;
 					try {
-		                Thread.sleep(10);
-		            } catch (InterruptedException e) {
-		                Thread.currentThread().interrupt();
-		            }
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						Thread.currentThread().interrupt();
+					}
 					System.out.println();
-					System.out.println();
-					System.out.println();
+					System.out.println(c.customerName + " bought a ticket and leaves.");
 					map.toString();
+					System.out.println();
 				}
 			}
 		}
--- Java Implementation/Seller.java
+++ Java Implementation/Seller.java
@@ -1,25 +1,31 @@
 import java.util.*;
 
 public class Seller implements Runnable {
-	ArrayList<Customer> listOfCustomers;
-	Timer t;
-	int numTicketsSold;
-	int totalCustomers;
-	int sellingTime;
-	int maxTimeToServe;
-	int minTimeToServe;
-	Seats map;
-	int row;
-	int col;
-	String name;
-	int turnedAway;
-	
+	public ArrayList<Customer> listOfCustomers;
+	public Timer t;
+	public int numTicketsSold;
+	public int totalCustomers;
+	public int sellingTime;
+	public int maxTimeToServe;
+	public int minTimeToServe;
+	public Seats map;
+	public int row;
+	public int col;
+	public String name;
+	public int turnedAway;
+
 	/**
 	 * Constructor for the Seller class.
-	 * @param numCustomers - the number of customer in the seller's queue.
-	 * @param mapOfSeats - the 2D array of seats the sellers will use to sell seats to the customer.
-	 * @param SellerName - the name of the seller.
-	 * @param timer - the time limit for the seller to know when to stop selling.
+	 * 
+	 * @param numCustomers
+	 *            - the number of customer in the seller's queue.
+	 * @param mapOfSeats
+	 *            - the 2D array of seats the sellers will use to sell seats to
+	 *            the customer.
+	 * @param SellerName
+	 *            - the name of the seller.
+	 * @param timer
+	 *            - the time limit for the seller to know when to stop selling.
 	 */
 	public Seller(int numCustomers, Seats mapOfSeats, String SellerName, Timer timer) {
 		turnedAway = 0;
@@ -38,7 +44,7 @@ public class Seller implements Runnable {
 		}
 		Collections.sort(listOfCustomers, new customerArrivalCompare());
 		for (int i = 0; i < listOfCustomers.size(); i++) {
-			listOfCustomers.get(i).customerName = SellerName + i;
+			listOfCustomers.get(i).customerName = SellerName + "-" + i;
 		}
 	}
 
@@ -60,36 +66,32 @@ public class Seller implements Runnable {
 			int currentTime = t.getElapsedTime();
 			if (!listOfCustomers.isEmpty() && map.soldAllSeats()) {
 				Customer current = listOfCustomers.get(0);
-				hold=current;
+				hold = current;
 				if (shouldSell(currentTime, current)) {
-					//System.out.println(name + " is currently working on sale" + " at time " + currentTime);
+					// System.out.println(name + " is currently working on sale"
+					// + " at time " + currentTime);
 					String time = "";
-					if(currentTime <10)
-					{
+					if (currentTime < 10) {
 						time = "00:0" + currentTime;
+					} else {
+						time = "00:" + currentTime;
 					}
-					else
-					{
-						time="00:" + currentTime;
-					}
-					System.out.println("Time: " + time+  " Seller: " + name);
-					sellTicket(maxTimeToServe, minTimeToServe, current);                //work for a time
+					System.out.println("Time: " + time + " Seller: " + name);
+					sellTicket(maxTimeToServe, minTimeToServe, current); // work
+																			// for
+																			// a
+																			// time
 				}
-				
-				
 
 			}
-			
-			
-		}
-		
-	
 
+		}
 
 	}
 
 	/**
 	 * Gets the number of customers that will be turned away.
+	 * 
 	 * @return - the number of customers turned away
 	 */
 	public int getNumTurned() {
@@ -98,14 +100,19 @@ public class Seller implements Runnable {
 	}
 
 	/**
-	 * Checks to see is a seat should be sold to the customer based on their arrival time.
-	 * @param currentMinute - the current time the customer arrives.
-	 * @param currentCustomer - the name of the customer.
-	 * @return - true or false which will determine if the ticket should be sold to the customer.
+	 * Checks to see is a seat should be sold to the customer based on their
+	 * arrival time.
+	 * 
+	 * @param currentMinute
+	 *            - the current time the customer arrives.
+	 * @param currentCustomer
+	 *            - the name of the customer.
+	 * @return - true or false which will determine if the ticket should be sold
+	 *         to the customer.
 	 */
 	public boolean shouldSell(int currentMinute, Customer currentCustomer) {
 		if (currentMinute >= currentCustomer.customerArrivalTime) {
-			//System.out.println(name + "\t" + listOfCustomers.size());
+			// System.out.println(name + "\t" + listOfCustomers.size());
 			return true;
 		}
 		return false;
@@ -113,26 +120,34 @@ public class Seller implements Runnable {
 
 	/**
 	 * The action of selling the ticket to the customer
-	 * @param minimumTime - the minimum selling time the seller can take to complete a transaction.
-	 * @param maximumTime - the maximum amount of time the seller can take to complete a transaction.
-	 * @param customer - the customer that the seller is currently serving.
+	 * 
+	 * @param minimumTime
+	 *            - the minimum selling time the seller can take to complete a
+	 *            transaction.
+	 * @param maximumTime
+	 *            - the maximum amount of time the seller can take to complete a
+	 *            transaction.
+	 * @param customer
+	 *            - the customer that the seller is currently serving.
 	 */
 	public void sellTicket(int minimumTime, int maximumTime, Customer customer) {
 		int range = maximumTime - minimumTime + 1;
 		sellingTime = minimumTime + (int) ((Math.random() * range));
 
-		int timeToSell = sellingTime + t.secondsElapsed;  // creates a random sale time depending on seller type
+		int timeToSell = sellingTime + t.secondsElapsed; // creates a random
+															// sale time
+															// depending on
+															// seller type
 		System.out.println(t.secondsElapsed);
 		System.out.println(name + " is selling in " + timeToSell);
 		Integer tTS = new Integer(timeToSell);
 		Integer elapsed = new Integer(t.secondsElapsed);
 		System.out.println(elapsed.compareTo(tTS) == 0);
 
-
 	}
 
 	public void checkSeatMap(Customer c) {
-		//remove first customer on list
+		// remove first customer on list
 		boolean x = true;
 		for (int ro = 0; x; ro++) {
 			for (int co = 0; x; co++) {
@@ -147,7 +162,6 @@ public class Seller implements Runnable {
 	public String toString() {
 		return name;
 	}
-
 
 	/*
 	 * prints customers in list
